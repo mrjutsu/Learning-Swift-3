d1ad54582edf170f7dc1c6cd11b0d7c2ca062c69
David Rosillo <drr538@gmail.com>
2017-03-09 00:50:49 -0500

    For in loop

---
+++ collections_and_control_flow/Collections and Control Flow.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+var todo: [String] = [
+    "Finish corrections course",
+    "Buy groceries",
+    "Respond to emails",
+    "Pick up dry cleaning",
+    "Order books online",
+    "Mow the lawn"
+]
+
+for task in todo {
+    print(task)
+}
+
+
+
+
+
+
+
+
+
+
+
