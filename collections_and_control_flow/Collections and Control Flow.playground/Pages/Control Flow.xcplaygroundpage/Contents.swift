09ed610942e898cedef8c68c1cb60edba6427212
David Rosillo <drr538@gmail.com>
2017-03-09 01:10:56 -0500

    while and repeat loops

--- collections_and_control_flow/Collections and Control Flow.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
+++ collections_and_control_flow/Collections and Control Flow.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -23,6 +23,47 @@ for number in 1...10 {
     print("\(number) times 5 = \(number * 5)")
 }
 
+// While loop
+
+var x = 0
+
+while x <= 20 {
+    x += 1
+    print(x)
+}
+
+var index = 0
+
+while index < todo.count {
+    print(todo[index])
+    index += 1
+}
+
+// Repeat while
+
+var counter = 1
+
+while counter < 1 {
+    print("I'm inside the while loop")
+    counter += 1
+}
+
+repeat {
+    print("I'm inside the repeat loop")
+    counter += 1
+} while counter < 1
+
+// repeat is used to evaluate at least once the condition
+// while is used to repeat the statement until the condition is met
+
+
+
+
+
+
+
+
+
 
 
 
